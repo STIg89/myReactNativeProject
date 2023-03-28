ef6128596b9a5905915e59eb92a4647a2155d2a6
STIg89 <andreystig89@gmail.com>
2023-03-28 17:42:40 +0300

    up

---
+++ src/Screens/ AdditionalsScreens/MapScreen.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+
+export const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+    </View>
+  );
+};
